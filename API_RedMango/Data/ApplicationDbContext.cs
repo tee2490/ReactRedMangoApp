351c7f5d86f61adece9521a2c2333b148d74a51c
tee2490@hotmail.com <tee2490@hotmail.com>
2024-05-05 15:51:40 +0700

    Section 01: NET API - Setup Connection String and DBContext

---
+++ API_RedMango/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_RedMango.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=.;Database=TestRedMangoDB;TrustServerCertificate=True;Trusted_Connection=True;");
+        }
+    }
+}
