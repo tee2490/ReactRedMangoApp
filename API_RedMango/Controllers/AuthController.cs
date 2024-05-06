dbe259d60356069ba9e1e9562010716d6323d536
tee2490@hotmail.com <tee2490@hotmail.com>
2024-05-06 10:52:19 +0700

    Section 02 : NET API -  Auth Controller and Dependency Injection

---
+++ API_RedMango/Controllers/AuthController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_RedMango.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+        private ApiResponse _response;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private string secretKey;
+
+        public AuthController(ApplicationDbContext db, IConfiguration configuration,
+           UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _db = db;
+            secretKey = configuration.GetValue<string>("ApiSettings:Secret");
+            _response = new ApiResponse();
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+    }
+}
