4f66f69f04cd89b04a3f1e61a26efb51976daf44
tee2490@hotmail.com <tee2490@hotmail.com>
2024-05-06 15:03:16 +0700

    Section 03 : NET API – Create Order Header and Details Table

--- API_RedMango/Data/ApplicationDbContext.cs
+++ API_RedMango/Data/ApplicationDbContext.cs
@@ -119,5 +119,7 @@ namespace API_RedMango.Data
         public DbSet<MenuItem> MenuItems { get; set; }
         public DbSet<ShoppingCart> ShoppingCarts { get; set; }
         public DbSet<CartItem> CartItems { get; set; }
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+        public DbSet<OrderDetails> OrderDetails { get; set; }
     }
 }
