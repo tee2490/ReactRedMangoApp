ee43faf4511cb6f6d926e642df131b62b77b014d
tee2490@hotmail.com <tee2490@hotmail.com>
2024-05-06 10:41:04 +0700

    Section 02 : NET API -  Add Login and Register DTOs

---
+++ API_RedMango/Models/Dto/LoginRequestDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace API_RedMango.Models.Dto
+{
+    public class LoginRequestDTO
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
--- API_RedMango/Utility/SD.cs
+++ API_RedMango/Utility/SD.cs
@@ -3,5 +3,8 @@
     public static class SD
     {
         public const string ImgPath = "/images/menuitem";
+
+        public const string Role_Admin = "admin";
+        public const string Role_Customer = "customer";
     }
 }
