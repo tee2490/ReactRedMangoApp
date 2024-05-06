267619da2660f8f9c7af0102b0475542cf05c596
tee2490@hotmail.com <tee2490@hotmail.com>
2024-05-06 19:25:32 +0700

    Section 03 : NET API – Preparing Stripe Payment Endpoint

---
+++ API_RedMango/Controllers/PaymentController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+
+namespace API_RedMango.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+
+        protected ApiResponse _response;
+        private readonly IConfiguration _configuration;
+        private readonly ApplicationDbContext _db;
+        public PaymentController(IConfiguration configuration, ApplicationDbContext db)
+        {
+            _configuration = configuration;
+            _db = db;
+            _response = new();
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<ApiResponse>> MakePayment(string userId)
+        {
+            ShoppingCart shoppingCart = _db.ShoppingCarts
+                .Include(u => u.CartItems)
+                .ThenInclude(u => u.MenuItem).FirstOrDefault(u => u.UserId == userId);
+
+            if (shoppingCart == null || shoppingCart.CartItems == null || shoppingCart.CartItems.Count() == 0)
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.IsSuccess = false;
+                return BadRequest(_response);
+            }
+
+            #region Create Payment Intent
+
+
+            #endregion
+
+            _response.Result = shoppingCart;
+            _response.StatusCode = HttpStatusCode.OK;
+            return Ok(_response);
+        }
+
+    }
+}
