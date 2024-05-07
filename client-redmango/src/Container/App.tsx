b4e4637ab4d30c3c7ca1b7c53f83af0b0ae4f826
tee2490@hotmail.com <tee2490@hotmail.com>
2024-05-07 13:46:52 +0700

    Section 04 : Client - Better Structure

--- client-redmango/src/Container/App.tsx
+++ client-redmango/src/Container/App.tsx
@@ -1,23 +1,13 @@
 import React,{useState,useEffect} from "react";
 import { Footer, Header } from "../Components/Layout";
-import { menuItemModel } from "../Interfaces";
+import { Home } from "../Pages";
 
 function App() {
-  const [menuItems, setMenuItems] = useState<menuItemModel[]>([]);
-
-  useEffect(() => {
-    fetch('https://localhost:44305/api/MenuItem')
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setMenuItems(data.result);
-      });
-  }, []);
 
   return (
     <div>
       <Header />
-      Main Component
+      <Home/>
       <Footer />
     </div>
   );
