2bd09f41d01ac15ff3997053622180bb0eae0496
tee2490@hotmail.com <tee2490@hotmail.com>
2024-05-07 11:53:28 +0700

    Section 04 : Fetch Menu Items & Interfaces in TS

--- client-redmango/src/Container/App.tsx
+++ client-redmango/src/Container/App.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import { Footer, Header } from "../Components/Layout";
+import { menuItemModel } from "../Interfaces";
 
 function App() {
+  const [menuItems, setMenuItems] = useState<menuItemModel[]>([]);
+
+  useEffect(() => {
+    fetch('https://localhost:44305/api/MenuItem')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setMenuItems(data.result);
+      });
+  }, []);
+
   return (
     <div>
       <Header />
