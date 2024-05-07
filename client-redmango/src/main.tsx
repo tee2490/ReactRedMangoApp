4d7c49787973b3e810948b29d2069fee38400077
tee2490@hotmail.com <tee2490@hotmail.com>
2024-05-07 18:25:13 +0700

    Section 04 : Client - Add Redux Toolkit and Create menuItemSlice to Project

--- client-redmango/src/main.tsx
+++ client-redmango/src/main.tsx
@@ -1,13 +1,16 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "../src/Container/App";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./Redux/store";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
