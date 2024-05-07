e91298f78947b574e4fcf2e7f9ef36acdfe11b90
tee2490@hotmail.com <tee2490@hotmail.com>
2024-05-07 10:29:08 +0700

    Section 04 : Install Bootstrap and Bootstrap Icons

--- client-redmango/src/main.tsx
+++ client-redmango/src/main.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '../src/Container/App'
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <App />
