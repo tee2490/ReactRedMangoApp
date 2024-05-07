c15ddb4ef55a967c0a03a1d95092a8ef7e674678
tee2490@hotmail.com <tee2490@hotmail.com>
2024-05-07 15:35:52 +0700

    Section 04 : Client - Add Routing and Not Found

--- client-redmango/src/Container/App.tsx
+++ client-redmango/src/Container/App.tsx
@@ -1,13 +1,17 @@
-import React,{useState,useEffect} from "react";
 import { Footer, Header } from "../Components/Layout";
-import { Home } from "../Pages";
+import { Home, NotFound } from "../Pages";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-
   return (
     <div>
       <Header />
-      <Home/>
+      <div className="pb-5">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </div>
       <Footer />
     </div>
   );
--- client-redmango/src/main.tsx
+++ client-redmango/src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from '../src/Container/App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "../src/Container/App";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
     <App />
-)
+  </BrowserRouter>
+);
