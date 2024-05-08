c1b9623e469f9ee39f66ed3021baf0a57d9cb23d
tee2490@hotmail.com <tee2490@hotmail.com>
2024-05-08 19:10:12 +0700

    Section 06 : Client –  Authentication Slice and User Interface

--- client-redmango/src/Interfaces/index.ts
+++ client-redmango/src/Interfaces/index.ts
@@ -1,5 +1,11 @@
 import menuItemModel from "./menuItemModel";
 import cartItemModel from "./cartItemModel";
 import shoppingCartModel from "./shoppingCartModel";
+import userModel from "./userModel";
 
-export type { menuItemModel, cartItemModel, shoppingCartModel };
+export type {
+    menuItemModel,
+    cartItemModel,
+    shoppingCartModel,
+    userModel
+};
