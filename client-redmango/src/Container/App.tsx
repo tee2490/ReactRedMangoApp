2e2deec7e3e14c4f155596d6fc1adefd522ea661
tee2490@hotmail.com <tee2490@hotmail.com>
2024-05-08 11:51:12 +0700

    Section 05 : Client –  Get Shopping Cart and Store in Slice

--- client-redmango/src/Container/App.tsx
+++ client-redmango/src/Container/App.tsx
@@ -1,8 +1,25 @@
 import { Footer, Header } from "../Components/Layout";
 import { Home, MenuItemDetails, NotFound } from "../Pages";
 import { Route, Routes } from "react-router-dom";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useGetShoppingCartQuery } from "../Apis/shoppingCartApi";
+import { setShoppingCart } from "../Redux/shoppingCartSlice";
 
 function App() {
+  const dispatch = useDispatch();
+
+  const { data, isLoading } = useGetShoppingCartQuery(
+    "94bb0b08-462f-4a95-8545-940b187588f2"
+  );
+
+  useEffect(() => {
+    if (!isLoading) {
+      console.log(data.result);
+      dispatch(setShoppingCart(data.result?.cartItems));
+    }
+  }, [data]);
+
   return (
     <div>
       <Header />
