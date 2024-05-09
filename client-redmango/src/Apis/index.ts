6852a2f0d3a802820c7fac994fb4f08d2f975512
tee2490@hotmail.com <tee2490@hotmail.com>
2024-05-09 14:24:22 +0700

    Section 07 : Client – Install React Stripe and Create Payment API

--- client-redmango/src/Apis/index.ts
+++ client-redmango/src/Apis/index.ts
@@ -1,5 +1,11 @@
 import authApi from "./authApi";
 import menuItemApi from "./menuItemApi";
+import paymentApi from "./paymentApi";
 import shoppingCartApi from "./shoppingCartApi";
 
-export { menuItemApi, shoppingCartApi, authApi };
+export {
+    menuItemApi,
+    shoppingCartApi,
+    authApi,
+    paymentApi
+};
