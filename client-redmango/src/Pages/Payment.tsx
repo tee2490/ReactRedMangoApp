e512c170dd4962c363663fb02114d126f5b74a57
tee2490@hotmail.com <tee2490@hotmail.com>
2024-05-09 16:54:51 +0700

    Section 07 : Client – Stripe Card Demo

--- client-redmango/src/Pages/Payment.tsx
+++ client-redmango/src/Pages/Payment.tsx
@@ -1,14 +1,27 @@
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 import { useLocation } from "react-router-dom";
+import { PaymentForm } from "../Components/Page/Payment";
+
+const stripePromise = loadStripe(
+  "pk_test_51M9JKILEJFIvBBF2XkujZkrsfTEzCwhb6Mju4cg46E92bFLupZh7FxanSKa17WqSqpfl3WuQ1K3AL2VbK1wCrg9200hagD6XvF"
+);
 
 function Payment() {
   const {
     state: { apiResult, userInput },
   } = useLocation();
 
-  console.log(apiResult);
-  console.log(userInput);
+  const options = {
+    // passing the client secret obtained from the server
+    clientSecret: apiResult.clientSecret,
+  };
 
-  return <div>Payment</div>;
+  return (
+    <Elements stripe={stripePromise} options={options}>
+      <PaymentForm />
+    </Elements>
+  );
 }
 
 export default Payment;
