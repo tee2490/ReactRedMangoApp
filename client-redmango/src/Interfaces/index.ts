85c21e7b957246bbc414e24de402e1ff4d2d63b0
tee2490@hotmail.com <tee2490@hotmail.com>
2024-05-10 14:14:55 +0700

    Section 08 : Client – Order Interface

--- client-redmango/src/Interfaces/index.ts
+++ client-redmango/src/Interfaces/index.ts
@@ -3,8 +3,12 @@ import cartItemModel from "./cartItemModel";
 import shoppingCartModel from "./shoppingCartModel";
 import userModel from "./userModel";
 import apiResponse from "./apiResponse";
+import orderDetailModel from "./orderDetailModel";
+import orderHeaderModel from "./orderHeaderModel";
 
 export type {
+    orderDetailModel,
+    orderHeaderModel,
     menuItemModel,
     cartItemModel,
     shoppingCartModel,
