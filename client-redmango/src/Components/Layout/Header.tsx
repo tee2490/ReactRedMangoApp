8377ccb0daf3da340c11aef089f8f45f5b56be0d
tee2490@hotmail.com <tee2490@hotmail.com>
2024-05-10 17:04:04 +0700

    Section 08 : Client –  Manage Orders Header based on Role

--- client-redmango/src/Components/Layout/Header.tsx
+++ client-redmango/src/Components/Layout/Header.tsx
@@ -4,6 +4,7 @@ import { cartItemModel, userModel } from "../../Interfaces";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../Redux/store";
 import { emptyUserState, setLoggedInUser } from "../../Redux/userAuthSlice";
+import { SD_Roles } from "../../Common/SD";
 
 function Header() {
   const dispatch = useDispatch();
@@ -47,6 +48,52 @@ function Header() {
                   Home
                 </NavLink>
               </li>
+
+              {userData.role == SD_Roles.ADMIN ? (
+                <li className="nav-item dropdown">
+                  <a
+                    className="nav-link dropdown-toggle"
+                    href="#"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Admin Panel
+                  </a>
+                  <ul className="dropdown-menu">
+                    <li
+                      style={{ cursor: "pointer" }}
+                      className="dropdown-item"
+                      onClick={() => navigate("order/myorders")}
+                    >
+                      My Orders
+                    </li>
+                    <li
+                      style={{ cursor: "pointer" }}
+                      className="dropdown-item"
+                      onClick={() => navigate("order/allOrders")}
+                    >
+                      All Orders
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Something else here
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+              ) : (
+                <li className="nav-item">
+                  <NavLink
+                    className="nav-link"
+                    aria-current="page"
+                    to="/order/myorders"
+                  >
+                    Orders
+                  </NavLink>
+                </li>
+              )}
+
               <li className="nav-item">
                 <NavLink
                   className="nav-link"
@@ -56,48 +103,6 @@ function Header() {
                   <i className="bi bi-cart"></i>
                   {userData.id && `(${shoppingCartFromStore.length})`}
                 </NavLink>
-              </li>
-
-              <li className="nav-item">
-                <NavLink
-                  className="nav-link"
-                  aria-current="page"
-                  to="/order/myorders"
-                >
-                  Orders
-                </NavLink>
-              </li>
-
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="#"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Admin panel
-                </a>
-                <ul className="dropdown-menu">
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Action
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Another action
-                    </a>
-                  </li>
-                  <li>
-                    <hr className="dropdown-divider" />
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Something else here
-                    </a>
-                  </li>
-                </ul>
               </li>
 
               <div className="d-flex" style={{ marginLeft: "auto" }}>
