6596e9180c46aed693a189643eb7901689167afa
tee2490@hotmail.com <tee2490@hotmail.com>
2024-05-10 17:40:52 +0700

    Section 08 : Client – Back to Order Button

--- client-redmango/src/Components/Page/Order/OrderSummary.tsx
+++ client-redmango/src/Components/Page/Order/OrderSummary.tsx
@@ -1,9 +1,11 @@
+import { useNavigate } from "react-router-dom";
 import { getStatusColor } from "../../../Helper";
 import { cartItemModel } from "../../../Interfaces";
 import { orderSummaryProps } from "./orderSummaryProps";
 
 export default function OrderSummary({ data, userInput }: orderSummaryProps) {
   const badgeTypeColor = getStatusColor(data.status!);
+  const navigate = useNavigate();
 
   return (
     <div>
@@ -45,6 +47,11 @@ export default function OrderSummary({ data, userInput }: orderSummaryProps) {
             </h4>
           </div>
         </div>
+      </div>
+      <div className="d-flex justify-content-between align-items-center mt-3">
+        <button className="btn btn-secondary" onClick={() => navigate('/order/myorders')}>
+          Back to Orders
+        </button>
       </div>
     </div>
   );
