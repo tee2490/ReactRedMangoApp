cdb57f1b2982dd684bb6777f47827062174cd000
tee2490@hotmail.com <tee2490@hotmail.com>
2024-05-11 14:44:44 +0700

    Section 09 : Client – Category Filters in Action

--- client-redmango/src/Components/Page/Home/MenuItemList.tsx
+++ client-redmango/src/Components/Page/Home/MenuItemList.tsx
@@ -21,23 +21,49 @@ function MenuItemList() {
 
   useEffect(() => {
     if (data && data.result) {
-      const tempMenuArray = handleFilters(searchValue);
+      const tempMenuArray = handleFilters(selectedCategory, searchValue);
       setMenuItems(tempMenuArray);
     }
   }, [searchValue]);
 
-  const handleFilters = (search: string) => {
-    let tempMenuItems = [...data.result];
+  const handleCategoryClick = (i: number) => {
+    const buttons = document.querySelectorAll(".custom-buttons");
+    let localCategory;
+    buttons.forEach((button, index) => {
+      if (index === i) {
+        button.classList.add("active");
+        if (index === 0) {
+          localCategory = "All";
+        } else {
+          localCategory = categoryList[index];
+        }
+        setSelectedCategory(localCategory);
+        const tempArray = handleFilters(localCategory, searchValue);
+        setMenuItems(tempArray);
+      } else {
+        button.classList.remove("active");
+      }
+    });
+  };
+
+  const handleFilters = (category: string, search: string) => {
+    let tempArray =
+      category === "All"
+        ? [...data.result]
+        : data.result.filter(
+            (item: menuItemModel) =>
+              item.category.toUpperCase() === category.toUpperCase()
+          );
 
     //search functionality
     if (search) {
-      const tempSearchMenuItems = [...tempMenuItems];
-      tempMenuItems = tempSearchMenuItems.filter((item: menuItemModel) =>
+      const tempSearchMenuItems = [...tempArray];
+      tempArray = tempSearchMenuItems.filter((item: menuItemModel) =>
         item.name.toUpperCase().includes(search.toUpperCase())
       );
     }
 
-    return tempMenuItems;
+    return tempArray;
   };
 
   useEffect(() => {
@@ -66,6 +92,7 @@ function MenuItemList() {
           {categoryList.map((categoryName, index) => (
             <li className="nav-item" key={index}>
               <button
+                onClick={() => handleCategoryClick(index)}
                 className={`nav-link p-0 pb-2 custom-buttons fs-5 ${
                   index === 0 && "active"
                 } `}
