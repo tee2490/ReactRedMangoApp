064cf474c4d2a0cfdb0012496d3c92613fdad83f
tee2490@hotmail.com <tee2490@hotmail.com>
2024-05-11 17:57:14 +0700

    Section 09 : Client – Input Controller Components

--- client-redmango/src/Pages/Order/AllOrders.tsx
+++ client-redmango/src/Pages/Order/AllOrders.tsx
@@ -2,9 +2,29 @@ import { withAdminAuth } from "../../HOC";
 import { useGetAllOrdersQuery } from "../../Apis/orderApi";
 import OrderList from "../../Components/Page/Order/OrderList";
 import { MainLoader } from "../../Common";
+import { SD_Status } from "../../Common/SD";
+import { useState } from "react";
+import { inputHelper } from "../../Helper";
+
+const filterOptions = [
+  "All",
+  SD_Status.CONFIRMED,
+  SD_Status.BEING_COOKED,
+  SD_Status.READY_FOR_PICKUP,
+  SD_Status.CANCELLED,
+];
 
 function AllOrders() {
   const { data, isLoading } = useGetAllOrdersQuery("");
+  const [filters, setFilters] = useState({ searchString: "", status: "" });
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    const tempValue = inputHelper(e, filters);
+    setFilters(tempValue);
+  };
+
   return (
     <>
       {isLoading && <MainLoader />}
@@ -17,9 +37,17 @@ function AllOrders() {
                 type="text"
                 className="form-control mx-2"
                 placeholder="Search Name, Email or Phone"
+                name="searchString"
+                onChange={handleChange}
               />
-              <select className="form-select w-50 mx-2">
-                <option value="All">All</option>
+              <select
+                className="form-select w-50 mx-2"
+                onChange={handleChange}
+                name="status"
+              >
+                {filterOptions.map((item) => (
+                  <option value={item === "All" ? "" : item}>{item}</option>
+                ))}
               </select>
               <button className="btn btn-outline-success">Filter</button>
             </div>
